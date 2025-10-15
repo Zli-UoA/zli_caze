6dd109518a4246094cc2f191e43356a07adb349a
39sho <g.39sho@gmail.com>
2025-10-16 00:49:08 +0900

    view側実装(適当だからあとで修正する)

--- src/app/routes/room.$roomId_.view.tsx
+++ src/app/routes/room.$roomId_.view.tsx
@@ -1,29 +1,31 @@
 import { Viewer } from "@/lib/viewer";
-import usePartySocket from "partysocket/react";
 import { useEffect, useRef } from "react";
-import * as schema from "../../schema/message";
 import type { Route } from "./+types/_app.room.$roomId";
+import { useLiveQuery } from "@tanstack/react-db";
+import { createCommentsCollection } from "@/components/collections/comments";
 
-export const loader = async ({ params }: Route.LoaderArgs) => {
-  return { roomId: params.roomId };
+export const clientLoader = async ({ params }: Route.LoaderArgs) => {
+  const roomId = params.roomId;
+
+  const commentsCollection = createCommentsCollection(roomId);
+
+  return { roomId, commentsCollection };
 };
 
 export default ({ loaderData }: Route.ComponentProps) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const viewRef = useRef<Viewer | null>(null);
+  const commentsRef = useRef<Set<string>>(new Set());
 
-  const ws = usePartySocket({
-    party: "room",
-    room: loaderData.roomId,
-    onMessage(e) {
-      if (viewRef.current == null) return;
-
-      const comment = schema.commentWithId.safeParse(JSON.parse(e.data));
-      if (comment.data == null) return;
-
-      viewRef.current.addMessage(comment.data.content);
-    },
-  });
+  const { data: comments } = useLiveQuery((q) =>
+    q
+      .from({ comment: loaderData.commentsCollection })
+      .fn.select(({ comment }) => ({
+        ...comment,
+        createdAt: new Date(comment.createdAt),
+      }))
+      .orderBy(({ comment }) => comment.createdAt, "desc"),
+  );
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -33,6 +35,22 @@ export default ({ loaderData }: Route.ComponentProps) => {
     viewRef.current = viewer;
     viewer.start();
   }, []);
+
+  useEffect(() => {
+    if (viewRef.current == null) return;
+
+    const newCommentsIdsSet = new Set<string>(comments.map((c) => c.id));
+    const diff = commentsRef.current.symmetricDifference(newCommentsIdsSet);
+
+    for (const commentId of diff) {
+      const comment = comments.find((c) => c.id === commentId);
+      if (comment == null) continue;
+
+      viewRef.current.addMessage(comment.content);
+    }
+
+    commentsRef.current = new Set<string>(comments.map((c) => c.id));
+  }, [comments]);
 
   return (
     <canvas ref={canvasRef} width={1920} height={1080} className="w-full" />
